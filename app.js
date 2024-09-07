ffec64ea23a9f431aa9d66d40471f4883b952082
dineshkumar <dineshjaipur22@gmail.com>
2024-09-07 21:53:20 +0530

    connected Frontend

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ const app=express()
 dotenv.config({path:"./config/config.env"});
 
 
-// app.use(cors({
-//     origin:[process.env.FRONTEND_URL],
-//     methods:['GET','POST','PUT','DELETE'],
-//     credentials:true
-// }));
+app.use(cors({
+    origin: 'https://job-seeker-client.vercel.app',
+    methods:['GET','POST','PUT','DELETE'],
+    credentials:true
+}));
 
-app.use(cors());
+// app.use(cors());
 
 app.use(cookieParser());
 app.use(express.json());
